718868f551059c5c4093f261f2a0e0f5666be677
Goga Octavian <ttgoga@yahoo.com>
2020-05-25 20:40:57 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int cautCar(char sir[], char valoare);
+
+int main()
+{
+    char vocale[100] = "aeiou";
+    char consoane[100] = "qwrtypsdfghjklzxcvbnm";
+    char val;
+
+    cout << "Introduceti un caracter: "; cin >> val;
+
+    val = tolower(val);
+
+    if ( cautCar(vocale, val) )
+        cout << "Ati introdus o vocala!" << endl;//A returnat 1
+    else if ( cautCar(consoane, val) )
+        cout << "Ati introdus o consoana!" << endl;//A returnat 1
+    else
+        cout << "Ati introdus un semn special!";
+    return 0;
+}
+
+int cautCar(char sir[], char valoare)
+{
+    int i, contor = 0;
+
+    for (i = 0; i < strlen(sir); i++)
+        if (sir[i] == valoare)
+            contor++;
+        return contor;
+}
