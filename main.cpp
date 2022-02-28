d18a37bdb31aafd0ad9eddff6cb4f8db2c864938
Goga <ttgoga@yahoo.com>
2022-02-28 13:14:28 +0200

    Commit

--- main.cpp
+++ main.cpp
@@ -16,9 +16,9 @@ int main()
     val = tolower(val);
 
     if ( cautCar(vocale, val) )
-        cout << "Ati introdus o vocala!" << endl;//A returnat 1
+        cout << "Ati introdus o vocala!" << endl;//Returned 1
     else if ( cautCar(consoane, val) )
-        cout << "Ati introdus o consoana!" << endl;//A returnat 1
+        cout << "Ati introdus o consoana!" << endl;//Returned 1
     else
         cout << "Ati introdus un semn special!";
     return 0;
@@ -31,5 +31,5 @@ int cautCar(char sir[], char valoare)
     for (i = 0; i < strlen(sir); i++)
         if (sir[i] == valoare)
             contor++;
-        return contor;
+    return contor;
 }
